aed727002f0c3c630fe6bd76612cae048aa8ec21
raffibromabar-web <raffibromabar@gmail.com>
2026-05-04 15:46:03 +0700

    menampilkan info ke layar

--- staticmember.cpp
+++ staticmember.cpp
@@ -20,3 +20,9 @@ int Mahasiswa::nim = 10;
 void Mahasiswa::setID() {
     id = ++nim;
 };
+
+void Mahasiswa::printAll(){
+    cout << "ID : " << id << endl;
+    cout << "Nama : " << nama << endl;
+    cout << endl;
+};
